6f67d59402ad67fe9973fcf8d07ddaca59ea787c
Yi Yang @ Cikavac <yangyi@megvii.com>
2016-12-27 15:26:26 +0800

    Remove bazel 0.4.0 from get_bazel.sh

--- utils/bazel/get_bazel.sh
+++ utils/bazel/get_bazel.sh
@@ -2,15 +2,11 @@ if ( java -version 2>&1 | grep "1.7" )
 then
     echo "Choose a Bazel version (1-3):"
     echo "  1 - 0.4.1-megvii2-jdk7"
-    echo "  2 - 0.4.0-megvii1-jdk7"
-    echo "  3 - 0.3.1-megvii4-jdk7"
+    echo "  2 - 0.3.1-megvii4-jdk7"
     read option
     if [ "${option}" == 1 ]
     then
         wget http://brain-ftp.megvii-inc.com/bazel-0.4.1-megvii2-jdk7 -O bazel
-    elif [ "${option}" == 2 ]
-    then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.0-megvii1-jdk7 -O bazel
     elif [ "${option}" == 3 ]
     then
         wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii4-jdk7 -O bazel
@@ -23,8 +19,7 @@ else
     echo "  1 - 0.4.3-megvii1-jdk8"
     echo "  2 - 0.4.2-megvii1-jdk8"
     echo "  3 - 0.4.1-megvii2-jdk8"
-    echo "  4 - 0.4.0-megvii1-jdk8"
-    echo "  5 - 0.3.1-megvii4-jdk8"
+    echo "  4 - 0.3.1-megvii4-jdk8"
     read option
     if [ "${option}" == 1 ]
     then
@@ -36,9 +31,6 @@ else
     then
         wget http://brain-ftp.megvii-inc.com/bazel-0.4.1-megvii2-jdk8 -O bazel
     elif [ "${option}" == 4 ]
-    then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.0-megvii1-jdk8 -O bazel
-    elif [ "${option}" == 5 ]
     then
         wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii4-jdk8 -O bazel
     else
