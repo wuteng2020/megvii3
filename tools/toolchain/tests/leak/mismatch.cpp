a45de457482c62820e967732bd74af4a6f00b3a3
Yi Yang @ Cikavac <yangyi@megvii.com>
2017-01-11 19:34:19 +0800

    An extra test for address sanitizer

---
+++ tools/toolchain/tests/leak/mismatch.cpp
@@ -0,0 +1,12 @@
+#include <memory>
+#include <cstring>
+
+int main()
+{
+    const char old_world[] = "Hello, world!";
+    char *new_world = new char[strlen(old_world) + 1];
+    strcpy(new_world, old_world);
+    free(new_world);
+
+    return 0;
+}
