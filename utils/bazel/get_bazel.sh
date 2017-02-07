939528c6491b7caa831f729470ac3b7904f05450
Yi Yang @ Cikavac <yangyi@megvii.com>
2017-02-07 16:52:05 +0800

    Rebuild Bazel using officially recommended command lines. These versions should be less buggy than previous versions, especially the jdk7 variants.

--- utils/bazel/get_bazel.sh
+++ utils/bazel/get_bazel.sh
@@ -2,39 +2,39 @@
 
 if ( java -version 2>&1 | grep "1.7" )
 then
-    echo "Choose a Bazel version (1-2):"
-    echo "  1 - 0.4.1-megvii2-jdk7"
-    echo "  2 - 0.3.1-megvii4-jdk7"
+    echo "Choose a Bazel version (1-3):"
+    echo "  1 - 0.4.4-megvii1-jdk7"
+    echo "  2 - 0.4.3-megvii2-jdk7"
+    echo "  3 - 0.3.1-megvii5-jdk7"
     read option
     if [ "${option}" == 1 ]
     then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.1-megvii2-jdk7 -O bazel
+        wget http://brain-ftp.megvii-inc.com/bazel-0.4.4-megvii1-jdk7 -O bazel
     elif [ "${option}" == 2 ]
     then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii4-jdk7 -O bazel
+        wget http://brain-ftp.megvii-inc.com/bazel-0.4.3-megvii2-jdk7 -O bazel
+    elif [ "${option}" == 3 ]
+    then
+        wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii5-jdk7 -O bazel
     else
         echo "Invalid option"
         exit 1
     fi
 else
-    echo "Choose a Bazel version (1-4):"
-    echo "  1 - 0.4.3-megvii1-jdk8"
-    echo "  2 - 0.4.2-megvii1-jdk8"
-    echo "  3 - 0.4.1-megvii2-jdk8"
-    echo "  4 - 0.3.1-megvii4-jdk8"
+    echo "Choose a Bazel version (1-3):"
+    echo "  1 - 0.4.4-megvii1-jdk8"
+    echo "  2 - 0.4.3-megvii2-jdk8"
+    echo "  3 - 0.3.1-megvii5-jdk8"
     read option
     if [ "${option}" == 1 ]
     then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.3-megvii1-jdk8 -O bazel
+        wget http://brain-ftp.megvii-inc.com/bazel-0.4.4-megvii1-jdk8 -O bazel
     elif [ "${option}" == 2 ]
     then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.2-megvii1-jdk8 -O bazel
+        wget http://brain-ftp.megvii-inc.com/bazel-0.4.3-megvii2-jdk8 -O bazel
     elif [ "${option}" == 3 ]
     then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.4.1-megvii2-jdk8 -O bazel
-    elif [ "${option}" == 4 ]
-    then
-        wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii4-jdk8 -O bazel
+        wget http://brain-ftp.megvii-inc.com/bazel-0.3.1-megvii5-jdk8 -O bazel
     else
         echo "Invalid option"
         exit 1
